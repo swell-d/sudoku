3e41b5f47d7f495b6d2368809225f947d4e62ff1
swell-d <3952800@gmail.com>
2021-11-23 21:31:05 +0100

    Start work with get_line function

--- sudoku.py
+++ sudoku.py
@@ -12,6 +12,10 @@ def read_xlsx(filename):
     return result
 
 
+def get_line(data, line):
+    return [None] * 9
+
+
 class SudokuTests(unittest.TestCase):
     sample_data = [None, None, None, None, None, 1, None, 8, None,
                    5, None, 4, None, 2, None, None, 1, None,
@@ -31,6 +35,12 @@ class SudokuTests(unittest.TestCase):
 
     def test_read_xlsx_return_sample(self):
         self.assertEqual(read_xlsx(r'2021-11-23.xlsx'), self.sample_data)
+
+    def test_get_line_return_something(self):
+        self.assertIsNotNone(get_line(self.sample_data, 1))
+
+    def test_get_line_return_9_numbers(self):
+        self.assertEqual(len(get_line(self.sample_data, 1)), 9)
 
 
 if __name__ == '__main__':
