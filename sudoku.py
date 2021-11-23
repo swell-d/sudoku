ed3ce143fb9680e30635defe916b16c5846cc8cc
swell-d <3952800@gmail.com>
2021-11-23 21:26:50 +0100

    Read xlsx file and return simple data

--- sudoku.py
+++ sudoku.py
@@ -4,18 +4,33 @@ import openpyxl as openpyxl
 
 
 def read_xlsx(filename):
-    if filename is None:
-        return [None] * 9 * 9
+    result = []
     sheet = openpyxl.load_workbook(filename).active
-    return sheet
+    for row in range(1, 10):
+        for column in range(1, 10):
+            result.append(sheet.cell(row=row, column=column).value)
+    return result
 
 
 class SudokuTests(unittest.TestCase):
-    def test_read_xlsx_return_81_numbers(self):
-        self.assertEqual(len(read_xlsx(None)), 81)
+    sample_data = [None, None, None, None, None, 1, None, 8, None,
+                   5, None, 4, None, 2, None, None, 1, None,
+                   None, None, 3, None, None, None, 2, 5, None,
+                   4, 5, None, 9, None, None, None, None, None,
+                   7, 3, None, 1, None, 5, 4, 2, None,
+                   None, 8, None, 3, 7, None, None, None, None,
+                   None, None, None, None, 9, None, 8, None, 2,
+                   None, 4, 8, None, None, 3, None, None, None,
+                   None, 6, 9, None, None, None, None, None, 5]
 
-    def test_read_xlsx_read_file(self):
+    def test_read_xlsx_return_something(self):
         self.assertIsNotNone(read_xlsx(r'2021-11-23.xlsx'))
+
+    def test_read_xlsx_return_81_numbers(self):
+        self.assertEqual(len(read_xlsx(r'2021-11-23.xlsx')), 81)
+
+    def test_read_xlsx_return_sample(self):
+        self.assertEqual(read_xlsx(r'2021-11-23.xlsx'), self.sample_data)
 
 
 if __name__ == '__main__':
