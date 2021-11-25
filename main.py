67206fc1064d71a39e7afd0b7cecca96cf3be600
swell-d <3952800@gmail.com>
2021-11-25 22:44:23 +0100

    add new game

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sudoku
 
 if __name__ == '__main__':
-    data = sudoku.read_xlsx(r'2021-11-24.xlsx')
+    data = sudoku.read_xlsx(r'2021-11-25.xlsx')
     new_data = sudoku.find_answer(data)
-    for _ in range(22):
+    for _ in range(30):
         new_data = sudoku.find_answer(new_data)
     sudoku.print_data_81(new_data)
