30bd0b4486250b5a10694f2566c934fcb536fd09
swell-d <3952800@gmail.com>
2021-11-26 22:29:43 +0100

    2021-11-26 new challenge

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sudoku
 
 if __name__ == '__main__':
     field = sudoku.Field()
-    field.read_xlsx(r'2021-11-25.xlsx')
-    for _ in range(11):
+    field.read_xlsx(r'2021-11-26.xlsx')
+    for _ in range(20):
         field.find_answer()
     field.print_field()
